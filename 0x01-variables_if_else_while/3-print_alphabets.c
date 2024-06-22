62fafce20364c432feee82242917713b0cdf3c1f
Madonna <Madoonna.Medhat@icloud.com>
2024-06-21 18:39:28 -0700

    ll

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 /**
- * main - This program prints the size of each data type
- * Return: 0
+ * main - Prints the alphabet in lowercase then upercase using putchar.
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	char c;
-	int d;
-	long int li;
-	long long int lli;
-	float f;
+int i;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+for (i = 0 ; i < 52 ; i++)
+{
+if (i < 26)
+putchar(i + 97);
+else
+putchar(i + 39);
+}
+putchar('\n');
 return (0);
 }
