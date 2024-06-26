38890b3b6632ddd7738ef724d693d7f5a28ed7cd
root <madonna.medhat@icloud.com>
2024-06-25 18:34:19 -0700

    kk

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,7 @@ int main(void)
 {
         char *str = "_putchar\n";
         int i;
-                for (i = 0; i++; str[i] !='\0');
+                for (i = 0; str[i] != '\0'; i++)
                 {
                 _putchar(str[i]);
                 }
