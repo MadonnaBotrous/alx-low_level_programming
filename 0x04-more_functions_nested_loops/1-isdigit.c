55e96bcafb9822c2b5799f96bf5a8fb20e1e9db8
root <madonna.medhat@icloud.com>
2024-06-27 10:41:56 -0700

    gg

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#nclude "main.h"
+
+/**
+ * int _isdigit checking  for a digit  (0 through 9)
+ * @c is character to be checked 
+ * if c is a digit return 1 | is else return o
+ */
+
+int _isdigit(int c)
+{
+c=0
+if (c > 47 && c < 58)
+return (1);
+else
+return (0);
+}
