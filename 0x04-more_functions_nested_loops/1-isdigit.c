187b4858715c5889732d97f42f371c31ade630a6
root <madonna.medhat@icloud.com>
2024-06-27 10:45:53 -0700

    hh

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,13 @@
-#nclude "main.h"
+#include "main.h"
 /**
  * _isdigit - checking if the paramater is a digit or not
  * @c: a character to be checked
  * Return: 1 if it s a digit - 0 if it's not
  */
+
 int _isdigit(int c)
 {
-if (c > 47 && c < 58)
+if (c >= 48 && c <= 57)
 return (1);
 else
 return (0);
