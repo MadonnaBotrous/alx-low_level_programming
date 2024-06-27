63ca4e8dd9daf8c7f8468f9fc13cae7b2d28dbb2
root <madonna.medhat@icloud.com>
2024-06-27 11:19:29 -0700

    kk

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_most_numbers - a function that prints most number , from 0 to 9 except 2&4 
+ *return void
+ */
+void print_most_numbers(void)
+{
+int i;
+
+for (i =48 ; i < 58 ; i++)
+{
+if (i != 50 && i != 52)
+_putchar (i);
+}
+_putchar ('\n');
+}
