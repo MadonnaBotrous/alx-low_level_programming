c4a186c43f918711d916e520cc6ad6c0a69c63b1
root <madonna.medhat@icloud.com>
2024-06-27 07:42:44 -0700

    gg

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+# include "main.h"
+
+/**
+ *checking for uppercase character
+ *@c: a character to be checked
+ *eturns 1 if c is uppercase | 0 if else
+ */
+int _isupper(int c);
+{
+if (c > 64 && c < 91)
+return (1);
+else
+return (0);
+}
