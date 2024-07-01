39ef3fd57b40882ebb2dfeb15c3b6f313b6b5bfa
root <madonna.medhat@icloud.com>
2024-07-01 13:52:05 -0700

    hh

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,19 @@
 #include "main.h"
-
+/**
+ * Dereference the pointer and assign 98 to the value it points to
+ */
 void reset_to_98(int *n) 
 {
-    *n = 98;  // Dereference the pointer and assign 98 to the value it points to
+    *n = 98;
+}
+int main(void)
+{
+    int n;
+
+    printf("n = %d\n", n);  // Print the initial value of n (may be undefined)
+
+    reset_to_98(&n);
+    printf("n = %d\n", n);
+
+    return 0;
 }
