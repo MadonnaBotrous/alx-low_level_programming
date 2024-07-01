160d06f248c4a056f9b0b591b9ccbc8c6535a597
root <madonna.medhat@icloud.com>
2024-07-01 14:13:02 -0700

    hh

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - resets the value of an int var to 98
+ *reset_to_98 - resets the value of an int var to 98
  *@n: number to be reset
  */
 void reset_to_98(int *n) 
 {
-    *n = 98;
+*n = 98;
 }
