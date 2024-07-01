414da4e1677c7f73df4911540b5d8fe5215d9cc0
root <madonna.medhat@icloud.com>
2024-07-01 14:55:00 -0700

    hh

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * swap_int - a function that swaps the values of two integers
- * @a: the fisrt intiger to be swapped
- * @b: the second intiger to be swapped
+ *swap_int - a function that swaps the values of two integers
+ *@a: the fisrt intiger to be swapped
+ *@b: the second intiger to be swapped
  */
 void swap_int(int *a, int *b)
 {
-int temp
+int temp;
 temp = *a;
 *a = *b;
 *b = temp;
