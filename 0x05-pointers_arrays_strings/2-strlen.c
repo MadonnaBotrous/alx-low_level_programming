d258ffecda0906bef7d07f896c7bab606076d507
root <madonna.medhat@icloud.com>
2024-07-01 16:43:38 -0700

    ll

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *strlen - the length of string to be shown
+ *@s: sting to be mesured
+ *return: value of i
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while(*(s+i) != 0)
+{
+i++
+}
+return('i');
+}
