6885beeb981ca3a43e821052d052d69a3ab41265
root <madonna.medhat@icloud.com>
2024-07-01 17:05:52 -0700

    hh

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *puts -  a function that prints a string
+ *@str: the string that will be printed
+ */
+void _puts(char *str)
+{
+int i = 0;
+while(*(s + i) != '\0')
+{
+_putchar (*(s + i));
+i++;
+}
+_putchar ('\n');
+}
