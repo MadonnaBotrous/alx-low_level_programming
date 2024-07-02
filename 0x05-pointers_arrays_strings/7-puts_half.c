c9d35cdec71459650f923ceb01261c27017c0e7d
root <madonna.medhat@icloud.com>
2024-07-01 18:12:38 -0700

    hh

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+ * puts_half - check code
+ * @str: string to be printed
+ *
+ */
+
+void puts_half(char *str)
+{
+int i  = 0, n, j;
+
+
+while (*(str + i) != '\0')
+{
+	i++;
+}
+
+if (i % 2 == 0)
+{
+	n = i / 2;
+}
+else
+{
+	n = (i + 1) / 2;
+}
+
+for (j = n; j < i; j++)
+{
+	_putchar(str[j]);
+}
+_putchar('\n');
+}
