c817868f43cad4fd4643bcdf59663deaf2b0de01
root <madonna.medhat@icloud.com>
2024-07-10 16:27:15 -0700

    jj

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * puts_recursion - prints a string using rec
- * @s : astring to be processed
- * return: void
+ * _puts_recursion - printing a string using rec
+ * @s: the string to be processed
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
-}
+{
 _putchar('\n');
 return;
-{
-_putchar(*s);
-puts_recursion(s + 1);
 }
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+
